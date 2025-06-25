815de1a15c811bb6db355cccc83fc6f3a14eceb9
max <Phillemon.Haikali@transnamib.com.na>
2025-06-25 19:31:29 +0200

    input_output learned

---
+++ input/output.py
@@ -0,0 +1,11 @@
+# The input() function is used to read a line of text input from the user.
+# It takes an optional prompt string as an argument, which is displayed to the user before waiting for input.
+
+name = input("What's your name? ")
+
+print("Hello, " + name + "!")
+
+# The output() function is used to display text or variables to the console.
+# It can take one or more arguments, which are separated by commas.
+
+print("Hello, world!" + name + "🤖")
---
+++ input_output.py
@@ -0,0 +1,8 @@
+name = input("What is your name?")
+age = input("Enter your age")
+profesion = input("What is your profesion?")
+
+
+print("your name is" +" " + name)
+print("your age is ", age)
+print("your profession is ", profesion)
