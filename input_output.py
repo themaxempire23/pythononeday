c6769afed7661eee7034f8d699d0f296c15e4085
max <Phillemon.Haikali@transnamib.com.na>
2025-06-25 19:36:44 +0200

    multi line comments

--- input_output.py
+++ input_output.py
@@ -6,3 +6,10 @@ profesion = input("What is your profesion?")
 print("your name is" +" " + name)
 print("your age is ", age)
 print("your profession is ", profesion)
+
+"""
+aurthor of project: Max
+test demo
+test demo
+this is a multiline comment
+"""
