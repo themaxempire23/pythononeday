11dce28c591e7feca550e578cf0d195a74829165
max <Phillemon.Haikali@transnamib.com.na>
2025-06-25 19:01:45 +0200

    variables

---
+++ variables&rulesToDeclareVariables.py
@@ -0,0 +1,12 @@
+# A Varibale is a name d reference to a valuem stored in memory.
+# Variables are used to store data that can be used and manipulated later in the program.
+
+x = 5
+
+name = "John"
+
+age = 30
+
+print(age)
+print(x)
+print(age, x, name )
