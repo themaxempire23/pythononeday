57d6a26961528b197431987428387f69ce0f9994
max <Phillemon.Haikali@transnamib.com.na>
2025-06-26 20:24:43 +0200

    data_types

---
+++ data_types.py
@@ -0,0 +1,28 @@
+# Example of numeric datatypes
+
+x = 100
+u = 2000
+b = 9
+e = 70
+z = 8 + 10
+
+print(x-e)
+print("\n You are awesome 😉")
+print(type(b-z))
+
+print(x*e%b+9)
+
+st = [23, 45, 78, 90, 100, 50, 88]
+print(st[5])
+
+# mutable and immutable
+
+tuple = (3,5,8,89,8)
+tuple[5]=26
+
+# dictionary (dict)
+
+dict = {"Name":"Tim Smithon", "age":89, "occupation":"Doctor"}
+print("[occupation]")
+print(dict["age"])
+print(dict["Name"])
