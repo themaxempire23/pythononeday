b629db4ba6952d27188f8ee7c569f9e57772b2e6
max <Phillemon.Haikali@transnamib.com.na>
2025-06-28 15:01:13 +0200

    continued

---
+++ section4/problem_data_types.py
@@ -0,0 +1,11 @@
+# string
+
+mystr = "this is a strting"
+print(mystr)
+
+marks = 4565285
+price = 56.50
+z = 5*8j # complex datatype
+
+print(price)
+print(type(marks))
---
+++ section4/problem_input_function.py
@@ -0,0 +1,6 @@
+'''
+Write a python program that takes a string input from the user and display it.
+'''
+
+user_name = input("enter your name")
+print("Welcome", user_name)
