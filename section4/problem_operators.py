dca5104cfcf7855b809d8cb8967eced6ec0d5884
max <Phillemon.Haikali@transnamib.com.na>
2025-07-19 16:48:21 +0200

    done with python operators

--- section4/problem_operators.py
+++ section4/problem_operators.py
@@ -3,26 +3,27 @@ Write a python program to demonstrate arithmatic and comparison operators
 '''
 
 a = 99   
-b = 23, 
+b = 23 
 u = 89
-x = 50,
-y = 78,
+x = 50
+y = 78
 t = 1 
 
 
 # Arthmetic operators
 print("Addition (+):", t+a )
 print("Subtraction (-):", a-u )
-print("Division (/) :", x/y )
+print("Division (/) :", x / y )
 print("Multiplication (*):", a * b )
-print("Modulus :",(a % b)) # The remainder operator
+print("Modulus :", x % t) # The remainder operator
 print("Exponents (**)", b ** t)
 
 # Comperison Operators 
 
-'''
+
 #Mostly used to compare to values
 print(u==t) # Result should be either true or false
 print(a>b)
 print(x<a)
-'''
+print(a!=b)
+
