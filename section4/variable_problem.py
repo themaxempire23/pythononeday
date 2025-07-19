63c7cfb7c549dfa39d78d9d7092f5fb73d93b421
max <Phillemon.Haikali@transnamib.com.na>
2025-07-19 16:59:35 +0200

    variable problem

---
+++ section4/variable_problem.py
@@ -0,0 +1,16 @@
+# Write a python program that declares a variable and print its value
+
+a = 100
+b = 23
+
+first_name = 'Smithon'
+last_name = 'Timothy'
+
+age = 56
+Gender = 'male'
+
+print( a + b)
+print(first_name)
+print(last_name)
+print(age)
+print(Gender)
