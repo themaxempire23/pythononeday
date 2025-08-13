aec48bb344b7a4bab1e32f77adf80a6104f9aa03
max <Phillemon.Haikali@transnamib.com.na>
2025-08-13 23:27:44 +0200

    operators

---
+++ section6/operators.py
@@ -0,0 +1,20 @@
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+result = num1 + num2
+
+print("Addition = ", (num1 + num2))
+print("Subtraction = ", (num1 - num2))
+print("Devision = ", (num1 / num2))
+print("Multiplication = ", (num1 * num2))
+
+#Comparison or relational operators -> result always true or fa;se
+
+print(num1 == num2)
+
+print(num1 > num2)
+
+print(num1 < num2)
+
+print(num1 != num2)
+
