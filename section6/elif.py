d756d1f8549d804053cc8bf5b2b001e721874174
max <Phillemon.Haikali@transnamib.com.na>
2025-08-22 00:28:50 +0200

    elif

---
+++ section6/elif.py
@@ -0,0 +1,28 @@
+# Elif statement: Used to check for multiple conditions and execute a block of code corresponding to the first true condition.
+
+"""
+Syntax:
+
+if conditiona1:
+    #code to execute if condition1 is true
+elif condition2:
+    #code to execute if condition1 is true
+else:
+   # code to execute if all conditions are false
+"""
+
+
+# Example:
+
+num = int(input('Enter a number: '))
+#name = 'Clutch'
+
+x = 0
+
+if x < 50:
+    print('x is positive')
+elif x < 50:
+    print('x is negative')
+else:
+    print('x is zero')    
+   
