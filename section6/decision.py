5d205b71be2c22541d84c0efb11dc1614f01c866
max <Phillemon.Haikali@transnamib.com.na>
2025-08-21 23:59:24 +0200

    if & else

---
+++ section6/decision.py
@@ -0,0 +1,26 @@
+
+# Decision making is the process of selecting an appropriate action or path to follow based on a given condition or set of conditions
+
+# Decision making code excutes statements or a set of statements when the condition is true 
+
+"""
+- If statement: used to execute a block of code only if a certain condition is true.
+- If else statement: used to execute on block of code if a condition is true, and another block if the condition is false
+   Syntax:
+   if condition:
+       # code to execute if condition is true
+
+     else:
+      # code to execute if condition is false  
+"""
+
+
+# Getting input from a user
+
+num = int(input('Enter a number: '))
+
+if num > 0:
+    print('Number is greater than zero!')
+
+else:
+    print('Number is smaller than zero..')    
