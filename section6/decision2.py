b174c078992d5b11af80ad9fdcbabfd34b5c2600
max <Phillemon.Haikali@transnamib.com.na>
2025-08-22 00:03:53 +0200

    getting marks using if alese statement

---
+++ section6/decision2.py
@@ -0,0 +1,7 @@
+# Getting learners scores
+
+marks = int(input('Enter your test mark: '))
+if marks >= 90:
+    print('You a distinction!')
+else: 
+    print('You do not have a distinction..Try again!')    
