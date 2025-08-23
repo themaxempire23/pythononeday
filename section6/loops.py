e96b1ee39926fa6fc73560b8d9b1e2a5781d92b5
max <Phillemon.Haikali@transnamib.com.na>
2025-08-23 13:45:57 +0200

    for loops are fun

---
+++ section6/loops.py
@@ -0,0 +1,26 @@
+# Loop: Programming constructor that allows a block of code to executed reapeatedly
+
+# Types of Loops:
+""" 1. For Loop: Used to iterate over a sequence
+             - such as list, tuple, or string and execute a block for each element in the sequence.
+"""
+#Practicing forloops
+
+# name = 'Tim Smithon'
+
+# for i in name:
+#     print(i)
+    
+
+# Printing a list, to list items
+
+lst = [1,2,3,4]
+for i in lst:
+    print(i)    
+
+# Example of dictionary
+
+dic = {"name:":"Tim", "surname:":"smithon", "Age:":32, "subject:":"Artificial Intelligence"}  
+
+for k,v in dic.items():
+    print(k,v)
