552d3e0e4bc4aa5bc08f531b65392e8d21aa935e
max <Phillemon.Haikali@transnamib.com.na>
2025-08-23 15:59:04 +0200

    while loops done

---
+++ section6/while_loops.py
@@ -0,0 +1,36 @@
+"""
+While loop is used to repeatedly execute a block of code as along as a certain condition is true.
+
+Syntax breakdown:
+
+while condition:
+
+   # code to be executed while condition is true
+"""
+
+# Example 1:
+
+"""  = 50
+
+ while x<100:
+     print(x)
+
+     x+=1 """
+
+
+# Example 2:
+
+"""i = 0
+while i < 5:
+    print('Tim Smithon is here....') #Prints this output 5 times
+    i += 1"""
+
+# Example 3:
+
+list = [1, 2, 3, 4, 5, 5, 99, 98, 97, 96, 95]
+    #print(len(list))
+i = 0
+while i< len(list):
+    print(list[i])
+    i += 1
+
