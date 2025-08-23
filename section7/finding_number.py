a19c07a2d6e53cb8a8bad073a51854c98dde451f
max <Phillemon.Haikali@transnamib.com.na>
2025-08-23 16:27:29 +0200

    finding number complete

---
+++ section7/finding_number.py
@@ -0,0 +1,9 @@
+# Determining if a number is positive or negative
+
+n = int(input("Enter a number: "))
+if n > 0:
+    print(n, "is a positive number...")
+elif n < 0:
+    print(n, "is a negative number")
+else:
+    print(n, "is a zero...")        
