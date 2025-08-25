e49cb70496d55a1bdae8b99e6cd8bee8187475b3
max <Phillemon.Haikali@transnamib.com.na>
2025-08-25 17:23:23 +0200

    numbers in python

---
+++ section9/numbers.py
@@ -0,0 +1,16 @@
+val1 = 4.3 #float
+val2 = 3 + 8j #complex number
+val3 = 32 #integer
+
+
+print(type(val2))
+print(type(val3))
+print(type(val3))
+
+# Mathematic operations with numbers
+
+print(val2+val1)
+print(val1/val2)
+print(val2-val1)
+print(val2*val1)
+print(val1%val2)
