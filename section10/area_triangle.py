3c7c300bdb7cf3ac4ec7f2f3ad0c2e8e636f5502
max <Phillemon.Haikali@transnamib.com.na>
2025-08-25 17:58:38 +0200

    area of triangle

---
+++ section10/area_triangle.py
@@ -0,0 +1,7 @@
+def area_triangle(base ,width):
+    area = base * width * 0.5
+    print("Area of the triangle is: ", area)
+
+base = float(input("Enter base value: "))
+width = float(input('Enter width value: '))
+area_triangle(base, width)
