df6face389fc091568baaa45e0ac4260c4cf69f6
max <Phillemon.Haikali@transnamib.com.na>
2025-08-25 18:04:12 +0200

    max() finding max value in alis

---
+++ section10/max_num.py
@@ -0,0 +1,6 @@
+# Fnding the maximum value in a list
+
+lst = [1, 99, 107, 88, 45, 32, 56, 32, 20, 78, 99]
+print(max(lst)) # max(lstp)
+
+# The max() function is used to find the largest value in the list
